77c8602ab92b4fce8a6f9db961186e42a7694d3b
Ottoniel Hapuc <101213615+OttoHapuc@users.noreply.github.com>
2023-01-25 19:40:30 -0300

    Update novaEntrada.js

--- src/components/entradaSaida/novaEntrada.js
+++ src/components/entradaSaida/novaEntrada.js
@@ -22,7 +22,7 @@ export default function NovaEntrada() {
             description,
             type: isInput
         };
-        await axios.post(`http://${REACT_APP_API_URL}/nova-entrada-saida`,obj,{headers:{user:user.name}})
+        await axios.post(`${REACT_APP_API_URL}/nova-entrada-saida`,obj,{headers:{user:user.name}})
         .then(res => {
             console.log(res);
             navigate("/home")
